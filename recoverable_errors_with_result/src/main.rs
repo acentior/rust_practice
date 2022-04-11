e465ba284abc253896bdf93e5337fd92f3c82b4e
acentior <acentior@outlook.com>
2022-04-12 01:48:07 +0400

    9.2 Recoverable errors with result - unwrap & expect

--- recoverable_errors_with_result/src/main.rs
+++ recoverable_errors_with_result/src/main.rs
@@ -2,18 +2,6 @@ use std::{fs::File, io::ErrorKind, panic};
 
 fn main() {
     let path = "hello.txt";
-    let f = File::open(path);
-
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create(path) {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => {
-                panic!("Problem opening the file: {:?}", other_error);
-            }
-        },
-    };
+    // let f = File::open(path).unwrap();
+    let f = File::open(path).expect(&format!("Failed to open {}", path));
 }
