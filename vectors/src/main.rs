6256d3b03197b63aa79593554a795c59d3830841
acentior <acentior@outlook.com>
2022-04-11 21:05:52 +0400

    8.1 vectors

---
+++ vectors/src/main.rs
@@ -0,0 +1,56 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+    // let v = vec![1, 2, 3];
+
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+
+    // let mut v = vec![1, 2, 3, 4, 5];
+
+    // let first = &v[0];
+
+    // v.push(6);
+    // println!("The first element is: {}", first);
+
+    // Iterating over the Values in a Vector
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+    println!("v is {:?}", v);
+
+    // Using an Enum to Store Multiple Types
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    println!("row is {:?}", row);
+}
